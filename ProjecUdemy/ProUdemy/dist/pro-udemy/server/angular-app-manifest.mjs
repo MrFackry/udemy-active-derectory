793ae596102fa1643561123196aad25a96638ae3
MunchiP <paulaviveusa@gmail.com>
2025-02-02 16:38:06 -0500

    moviendo el proyecto para arranche

--- ProjecUdemy/ProUdemy/dist/pro-udemy/server/angular-app-manifest.mjs
+++ ProjecUdemy/ProUdemy/dist/pro-udemy/server/angular-app-manifest.mjs
@@ -7,6 +7,10 @@ export default {
   routes: [
   {
     "renderMode": 2,
+    "route": "/"
+  },
+  {
+    "renderMode": 2,
     "route": "/Registrarse"
   },
   {
@@ -19,20 +23,17 @@ export default {
   },
   {
     "renderMode": 2,
-    "route": "/"
-  },
-  {
-    "renderMode": 2,
     "route": "/**"
   }
 ],
+  entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 24309, hash: 'fbeb4ab805dcece0f7f3d69700f87480f8e2b19320eb8e8a70d0ff6170c837b5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17883, hash: 'de4561a38819fd480a1a058827710c8830caa70837f2831486634cbff0234658', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'Iniciar-sesion/index.html': {size: 46617, hash: '815dfa2314e81a2371b78c9b0c904814b52b6821d92b120b4aaf8a9b3ac9d029', text: () => import('./assets-chunks/Iniciar-sesion_index_html.mjs').then(m => m.default)},
-    'Registrarse/index.html': {size: 49466, hash: '41ba4e1736bb4d9ea73357c05b3e6f24f38d34f5c95f018fe02cd53096bb0d4a', text: () => import('./assets-chunks/Registrarse_index_html.mjs').then(m => m.default)},
-    'InicioPrivado/index.html': {size: 44256, hash: '2de3b31c00af83f1fa03ce234b2e5096d983f800fa28565f4c78745aacc8800a', text: () => import('./assets-chunks/InicioPrivado_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 44334, hash: 'da65dccfdebce6e05b2cbff60bae398bba69ecedcd45e4298e91e7f2a16d11ca', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 24309, hash: 'a497bb77ef892caae58e72d6f0e14602a4dac0972c3c686f299e62898073d142', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17883, hash: 'b81c5b458e7c25e3cde63964b8b6294507faf623ecc3645f0d4520409fab7556', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'Iniciar-sesion/index.html': {size: 46584, hash: '30ac8e8bfff90d6789d3f99ef0927be5ff4756cacdf7f9a4413705937dfb57c7', text: () => import('./assets-chunks/Iniciar-sesion_index_html.mjs').then(m => m.default)},
+    'Registrarse/index.html': {size: 49433, hash: 'df8604f6b793be15c9ddf3dd5b4cd8db1383e3b947e010e380847917ebf53e63', text: () => import('./assets-chunks/Registrarse_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 44301, hash: 'ed898230283c9431c3c42f9f83a5145b443dbab1d71978d1c740fe67caa1dca3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'InicioPrivado/index.html': {size: 44223, hash: '4803b822b34ae87c3fdf192537b8b6630c7cd227f168a0dfc22946d9bc4bf545', text: () => import('./assets-chunks/InicioPrivado_index_html.mjs').then(m => m.default)},
     'styles-CXQUZ3PB.css': {size: 6979, hash: 'mYIPdabeAag', text: () => import('./assets-chunks/styles-CXQUZ3PB_css.mjs').then(m => m.default)}
   },
 };
